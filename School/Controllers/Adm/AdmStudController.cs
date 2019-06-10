cc1ed2fd116bf7a0ab693b1e04701f0deb9bd757
yasjbr <yaseenjaber@gmail.com>
2019-06-10 21:07:37 +0300

    Update AdmStudController.cs

--- School/Controllers/Adm/AdmStudController.cs
+++ School/Controllers/Adm/AdmStudController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using School.ServiceLayer.Services.AdmServices;
-
+//test 
 namespace School.Controllers.Adm
 {
     [Route("api/[controller]")]
